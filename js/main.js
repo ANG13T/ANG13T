9ab6c925cef215672b6627dbc9ec7821efb1e743
Angelina Tsuboi <angelina.t1832@gmail.com>
2021-11-22 10:08:40 -0800

    fix: preloader issue

--- js/main.js
+++ js/main.js
@@ -21,17 +21,17 @@
 	/*---------------------------------------------------- */
 	/* Preloader
 	------------------------------------------------------ */ 
-   $(window).load(function() {
+//    $(window).load(function() {
 
-      // will first fade out the loading animation 
-    	$("#loader").fadeOut("slow", function(){
+//       // will first fade out the loading animation 
+//     	$("#loader").fadeOut("slow", function(){
 
-        // will fade out the whole DIV that covers the website.
-        $("#preloader").delay(300).fadeOut("slow");
+//         // will fade out the whole DIV that covers the website.
+//         $("#preloader").delay(300).fadeOut("slow");
 
-      });       
+//       });       
 
-  	})
+//   	})
 
 
   	/*---------------------------------------------------- */
